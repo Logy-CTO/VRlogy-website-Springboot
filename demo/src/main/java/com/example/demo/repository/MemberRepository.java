8de024c96358aa477f90d5f67d241e9ec24d1b06
taiksu hong <74459097+Logy-CTO@users.noreply.github.com>
2023-10-03 22:01:05 +0900

    .

--- demo/src/main/java/com/example/demo/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.MemberInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberInfo, Integer> {
-
-    MemberInfo findByUsername(String username);
-}
